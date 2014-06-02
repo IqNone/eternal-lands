edb324a59266ba2ef41a5238f72ac51d4c50e947
Droid Manic <droid.manic@gmail.com>
2014-06-02 23:24:01 +0300

    Add remaining handler and changes in how to display text (to make bars smaller)

--- src/el/android/widgets/ELProgressBar.java
+++ src/el/android/widgets/ELProgressBar.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,6 +20,7 @@ import static java.lang.Math.min;
 
 public class ELProgressBar extends View {
     private boolean showText;
+    private boolean remaining;
     private int color;
 
     private int total = 100;
@@ -33,6 +35,7 @@ public class ELProgressBar extends View {
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ELProgressBar, 0, 0);
         try {
             showText = a.getBoolean(R.styleable.ELProgressBar_showText, false);
+            remaining = a.getBoolean(R.styleable.ELProgressBar_showText, false);
             color = a.getColor(R.styleable.ELProgressBar_color, 0);
         } finally {
             a.recycle();
@@ -62,6 +65,17 @@ public class ELProgressBar extends View {
 
     public void setShowText(boolean showText) {
         this.showText = showText;
+        invalidate();
+        requestLayout();
+    }
+
+
+    public boolean isRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(boolean remaining) {
+        this.remaining = remaining;
         invalidate();
         requestLayout();
     }
@@ -101,25 +115,45 @@ public class ELProgressBar extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        drawText(canvas);
         drawStroke(canvas);
         drawFill(canvas);
+        drawText(canvas);
     }
 
     private void drawText(Canvas canvas) {
+        int current_for_text;
+        float y;
+
+        if (remaining) {
+            current_for_text = total - current;
+        }
+        else {
+            current_for_text = current;
+        }
+
         if(showText) {
-            textPaint.setTextSize(getHeight() - 3);
-            canvas.drawText(String.valueOf(current), 2, textPaint.getTextSize(), textPaint);
+            if (current_for_text < 1000) {
+                textPaint.setTextSize(getHeight() - 3);
+            }
+            else {
+                textPaint.setTextSize(getHeight()/2 - 3);
+            }
+
+            y = textPaint.getTextSize() - 2;
+
+            canvas.drawText(String.valueOf(current_for_text), 2, y, textPaint);
         }
     }
 
     private void drawStroke(Canvas canvas) {
-        int left = showText ? 30 : 0;
+        //int left = showText ? 30 : 0;
+        int left = showText ? 0 : 0;
         canvas.drawRect(left + 1, 1, getWidth() - 1, getHeight() - 1, borderPaint);
     }
 
     private void drawFill(Canvas canvas) {
-        int left = showText ? 30 : 0;
+        //int left = showText ? 30 : 0;
+        int left = showText ? 0 : 0;
         int right = left + (int)(min(1, (double)current / total) * (getWidth() - left));
 
         if(right > left + 4) {
