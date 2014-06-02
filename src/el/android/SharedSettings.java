7c763ffe069c7c589b668ed581ef8b3e6eda78c4
Droid Manic <droid.manic@gmail.com>
2014-06-02 23:17:04 +0300

    Define max food amount for settings

--- src/el/android/SharedSettings.java
+++ src/el/android/SharedSettings.java
@@ -8,4 +8,5 @@ public class SharedSettings {
     public static final String REPORTS_INCLUDE_USERNAME = "reports_include_username";
     public static final String REPORTS_INCLUDE_EXTRA = "reports_include_extra";
     public static final String ENABLE_LONG_PRESS = "enable_long_press";
+    public static final String MAX_FOOD_AMOUNT = "max_food_amount";
 }
